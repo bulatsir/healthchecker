1723201aff656c01fb071602e17c78cd0b881958
Sirazev Bulat <sirazev@gmail.com>
2021-08-30 23:48:15 +0300

    adding telegram alerting if resource is Down

--- healthchecker.go
+++ healthchecker.go
@@ -4,11 +4,39 @@ import (
 	"bufio"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"gopkg.in/yaml.v2"
 )
+
+var Config Conf
+
+type Conf struct {
+	Key    string `yaml:"key"`
+	ChatId string `yaml:"chat_id"`
+}
+
+func sendMessage(s string, reply string) {
+	bot, err := tgbotapi.NewBotAPI(Config.Key)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	log.Printf("send message to %s", s)
+	chatId, _ := strconv.ParseInt(s, 10, 64)
+	msg := tgbotapi.NewMessage(chatId, reply)
+
+	bot.Send(msg)
+}
 
 //error handler
 
@@ -38,13 +66,19 @@ func periodiccheck() {
 		for scanner.Scan() {
 			url := scanner.Text()
 			resp, err := http.Get(url)
-			checkerror(err)
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
 			urlForCheck.Domain = url
 
 			if resp.StatusCode == 200 {
 				urlForCheck.Status = "up"
+
 			} else {
 				urlForCheck.Status = "down"
+				alertMessage := urlForCheck.Domain + " is down"
+				sendMessage(Config.ChatId, alertMessage)
+
 			}
 
 			log.SetOutput(logfile)
@@ -92,6 +126,16 @@ func healthcheck(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
+	//read configuration
+
+	yamlFile, err := ioutil.ReadFile("key.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &Config)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
 	go periodiccheck() //run ticker
 	http.HandleFunc("/", healthcheck)
 	http.ListenAndServe(":8090", nil)
